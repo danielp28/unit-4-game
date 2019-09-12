4547cfd5ac0f9024a821bbf4e7367a6f198f9481
Daniel Masters <daniel.p.masters@gmail.com>
2019-09-12 17:11:49 -0500

    DRY'd up my javascript for better readablility and load time

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,93 +1,49 @@
-$(document).ready(function () {
+
 
     var random = Math.floor(Math.random() * 101 + 19)
 
     $("#total-score").text(random);
 
-
-    var num1 = Math.floor(Math.random() * 11 + 1);
-    var num2 = Math.floor(Math.random() * 11 + 1);
-    var num3 = Math.floor(Math.random() * 11 + 1);
-    var num4 = Math.floor(Math.random() * 11 + 1);
-
-    var userScore = 0;
+    var userScore;
     var wins = 0;
     var losses = 0;
 
     $("#wins").text = wins;
     $("#losses").text = losses;
 
-    function reset() {
+    function startNewGame() {
         random = Math.floor(Math.random() * 101 + 19);
-        console.log(random)
+        for (i = 0; i < 4; i++) {
+            $("#image" + (i + 1)).attr("data-num", Math.floor(Math.random() * 11 + 1))
+        }
         $("#total-score").text(random);
-        num1 = Math.floor(Math.random() * 11 + 1);
-        num2 = Math.floor(Math.random() * 11 + 1);
-        num3 = Math.floor(Math.random() * 11 + 1);
-        num4 = Math.floor(Math.random() * 11 + 1);
         userScore = 0;
         $("#user-score").text(userScore);
     }
+
+    startNewGame();
 
     function win() {
         alert("You're gnarly!");
         wins++;
         $("#wins").text(wins);
-        reset();
+        startNewGame();
     }
     function lose() {
         alert("Not so gnarly");
         losses++;
         $("#losses").text(losses);
-        reset();
+        startNewGame();
     }
 
-    $("#image1").on("click", function () {
-        userScore = userScore + num1;
-        console.log(userScore);
+    $(".crystal").on("click", function () {
+        userScore += parseInt($(this).attr("data-num"));
+        var userWins = userScore === random
         $("#user-score").text(userScore);
-        if (userScore === random) {
+        if (userWins) {
             win();
         } else if (userScore > random) {
             lose();
         }
     })
-    $("#image2").on("click", function () {
-        userScore = userScore + num2;
-        console.log(userScore);
-        $("#user-score").text(userScore);
-        if (userScore === random) {
-            win();
-        } else if (userScore > random) {
-            lose();
-        }
-    })
-    $("#image3").on("click", function () {
-        userScore = userScore + num3;
-        console.log(userScore);
-        $("#user-score").text(userScore);
-        if (userScore === random) {
-            win();
-        } else if (userScore > random) {
-            lose();
-        }
-    })
-    $("#image4").on("click", function () {
-        userScore = userScore + num4;
-        console.log(userScore);
-        $("#user-score").text(userScore);
-        if (userScore === random) {
-            win();
-        } else if (userScore > random) {
-            lose();
-        }
-    })
-
-
-
-
-
-
-
-
-})
+ 
